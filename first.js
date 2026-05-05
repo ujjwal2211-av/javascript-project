0b6023f8659d0009167bbe8731fa8af73ab206c9
ujjwal2211-av <ujjwalkumawat123@gmail.com>
2026-05-06 01:28:10 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,150 @@
+// const profile = {               
+//     Username : "ujjwal kumawat",
+//      isFollow : true ,
+//      followers : 567895,
+//      following :5,
+//      age:21, 
+
+// };
+//  profile["Username"] ="AVAL" ;
+//  console.log(profile);
+//arithmatic operator
+// let a=5; // = assignment operaror
+// let b =4;
+/*
+console.log("a=",a,"& b=",b);
+console.log("a+b=" , a+b);
+console.log("a-b=" , a-b);
+console.log("a*b=" , a*b);
+console.log("a/b=" , a/b);
+console.log("a%b=" , a%b);
+console.log("a ** b=" , a ** b);
+console.log("++a", ++a);
+console.log("a = " , a);
+console.log("a++", a++);
+console.log("a = " , a); */
+
+// //Assignment opperator
+// a -= 5;
+// console.log("a =",a);
+// a += 5;
+// console.log("a =",a);
+// a *= 5;
+// console.log("a =",a);
+
+// a /= 5;
+// console.log("a =",a);
+
+//comparision operator 
+// let d =4;
+// let c =5;
+//   console.log("c !== d", c !== d);
+// console.log("d < c", d<);
+
+
+//prime number or odd
+// let num = 25;
+// if (num % 2 === 0)
+// {
+//     console.log(num,"is even");
+// } 
+// else {  
+//     console.log(num,"is odd");
+// }  
+
+//else if
+
+/*let mode="dark";
+let b;
+if(mode === "dark")
+{
+    b="black";
+}
+else if(mode === "light")
+{
+    b="blue";
+}
+else if(mode === "pink")
+{
+    b="pink";
+}
+else {
+    b="excellent"
+}
+console.log(b);*/
+
+// let num = prompt("enter a number:");
+
+// if(num % 5 === 0 )
+// {
+//     console.log(num,"is multiple of 5");
+// }
+// else
+// {
+//     console.log(num,"is not multiple of 5");
+// }
+
+// let score = prompt("enter yours marks");
+// let grade;
+
+// if(score >= 90 && score <= 100){
+//     grade="A";
+// }
+// else if(score >= 70 && score <= 89){
+//     grade="B";
+// }
+// else if(score >= 60 && score <= 69){
+//     grade="C";
+// }
+// else if(score >= 50 && score <= 59){
+//     grade="D";
+// }
+// else if(score >= 0 && score <= 49){
+// grade="F";
+// }
+// console.log("According to your score, your grade was :", grade );
+
+// let a ="ujjwal"
+// {
+//     let a= "this"
+//     console.log(a)
+// };
+
+// console.log(a);
+
+// var a ="ujjwal"
+// {
+// var  a= "this"
+//     console.log(a)
+// };
+
+// console.log(a)
+// const a ="ujj";
+// const b = 7;
+// console.log(a+b);
+// console.log(typeof (a+b));
+
+const a ={
+    Username : "ujjwal kumawat",
+   isFollow : true ,
+   followers : 567895,   
+   following :5,
+    age:21,
+};
+console.log(a)
+a.followers=123;
+console.log(a)
+a["following"]=5;
+console.log(a)
+a["age"]=a["age"] +2;
+console.log(a)
+a['friend']="krishna"
+console.log(a)
+
+const dict ={
+    appreciate:"motivate kkarna" ,
+    happy: "khush hona",
+    sad:"dukhi hona"
+};
+console.log(dict.sad);
+console.log(dict['happy']);
